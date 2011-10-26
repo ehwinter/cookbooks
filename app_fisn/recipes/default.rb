1a5d492068c953d20d71e652d1710cd787b9bb27
Ubuntu <ubuntu@ip-10-202-62-65.ec2.internal>
2011-10-26 21:25:10 +0000

    Build target via new role/app_fisn recipe.
    Squashed commit of the following:
    
    commit 43b89d7428ed1ddade78551d08f6a34903f3046d
    Author: Ubuntu <ubuntu@ip-10-202-62-65.ec2.internal>
    Date:   Wed Oct 26 20:59:16 2011 +0000
    
        wip: building target. Works on top of walkull, but not on its own.
    
    commit 20fafdfd5b7f502041df246e2fd2eaff7cc88327
    Author: Ubuntu <ubuntu@ip-10-202-62-65.ec2.internal>
    Date:   Wed Oct 26 19:15:59 2011 +0000
    
        wip: toTarget build

---
+++ app_fisn/recipes/default.rb
@@ -0,0 +1,146 @@
+#
+# Cookbook Name:: app_fisn
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Pull rsa-key-20110128 Wallkull Chef Server ssh key from the databag.
+# this key is autorized for git usage 
+# ------------------------------------------------------------
+
+db=data_bag('target')
+ssh=data_bag_item('target', 'ssh')
+bash "write_pem" do
+  user "root"
+  group "root"
+  cwd "/tmp"
+  code <<-EOS
+echo "#{ssh['pem']}" > /root/.ssh/id_rsa
+chmod 0400 /root/.ssh/id_rsa
+
+
+echo "#{ssh['pem']}" > /home/ubuntu/.ssh/id_rsa
+chmod 0400 /home/ubuntu/.ssh/id_rsa
+chown ubuntu /home/ubuntu/.ssh/id_rsa
+chgrp ubuntu /home/ubuntu/.ssh/id_rsa
+
+EOS
+end
+
+
+
+
+# write a file out in ~ with the name of this machine.
+if node.attribute?("ec2")
+  execute "identify_by_name" do
+    command "touch   fisn_server-#{node['ec2']['instance_id']}-$(date +%FT%H%M)"
+    cwd "/home/ubuntu"
+    user "ubuntu"
+    group "ubuntu"
+    not_if "ls /home/ubuntu/f* | grep fisn_server-"
+  end
+end
+
+
+
+
+# Create all directories (but not git directories)
+vanity="apps/vanityurls"
+%w(git git/fisn apps backups apps/vanityurls apps/vanityurls/401krollover.us  apps/vanityurls/bondinvestments.us  apps/vanityurls/depositline.com  apps/vanityurls/fisnonline.com  apps/vanityurls/iradeposit.com apps/fisn.com apps/fisn.com/fisn apps/fisn.com/fisn/current apps/aws.fisn.com apps/aws.fisn.com/fisn apps/aws.fisn.com/fisn/current apps/preview.fisn.com apps/preview.fisn.com/fisn apps/preview.fisn.com/fisn/current   ).each do |dir|
+  directory "/home/ubuntu/#{dir}/" do
+  owner "ubuntu"
+  group "ubuntu"
+  mode 0755
+  action :create
+  end
+end
+
+
+
+# ------------------------------
+# Clone git repos
+# ------------------------------
+
+jcloud="feedont@feedontheword.com:git/cloud"
+jfisn="feedont@feedontheword.com:git/fisn"
+gh="git@github.com:ehwinter"
+options="--depth 1"
+
+
+
+
+# Populate ~/fisn-chef-target
+%w(fisn-chef-target).each do |dir|
+  git "/home/ubuntu/#{dir}" do
+    user "ubuntu"
+    group "ubuntu"
+    repository "#{jfisn}/#{dir}"
+    reference "master"
+    action :sync
+  end
+end
+
+# Populate the ~/scripts directory
+%w(scripts).each do |dir|
+  git "/home/ubuntu/#{dir}" do
+    user "ubuntu"
+    group "ubuntu"
+    repository "#{jfisn}/#{dir}"
+    reference "master"
+    action :sync
+  end
+end
+
+# Populate the ~/git/* directories.
+%w(fisn scripts).each do |dir|
+#%w(scripts).each do |dir|
+  git "/home/ubuntu/git/fisn/#{dir}" do
+    user "ubuntu"
+    group "ubuntu"
+    repository "#{jfisn}/#{dir}"
+    reference "master"
+    action :sync
+  end
+end
+
+
+# Populate the ~/apps/* directories.
+%w(fisn.com aws.fisn.com preview.fisn.com).each do |domain|
+  bash "copy_fisn_domains" do
+    user "ubuntu"
+    group "ubuntu"
+    cwd "/tmp"
+    not_if "ls -a /home/ubuntu/apps/#{domain}/fisn/current/fisn | grep .git"
+    code <<-EOS
+cp -pR /home/ubuntu/git/fisn/fisn /home/ubuntu/apps/#{domain}/fisn/current/
+EOS
+  end
+end
+
+%w(original.fisn.com).each do |dir|
+  git "/home/ubuntu/apps/#{dir}" do
+    user "ubuntu"
+    group "ubuntu"
+    repository "#{jfisn}/#{dir}"
+    reference "master"
+    action :sync
+  end
+end
+
+
+# ------------------------------
+# Copy files as appropriate
+# ------------------------------
+
+
+
+# refernce .bashrc - this is needed to setup aws_keys
+template "/home/ubuntu/.bashrc" do
+  source "bashrc.erb"
+  owner "ubuntu"
+  group  "ubuntu"
+  mode "0750"
+end
